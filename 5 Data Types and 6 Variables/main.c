5f6539deeb00959bdec2ef17cf325259dc84ec8b
Lukas Einhaus <lukas.einhaus@uni-due.de>
2020-05-26 13:32:02 +0200

    add week6 solution

--- 5 Data Types and 6 Variables/main.c
+++ 5 Data Types and 6 Variables/main.c
@@ -1,12 +1,33 @@
 #include <stdbool.h>
 #include "exercise.h"
 
+void
+printFloat(ieee_float64 number)
+{
+    printf("sign: 0x%x, exponent: 0x%x - %i, mantisse: 0x%lx\n",
+            number.fields.sign,
+            number.fields.exponent,
+            number.fields.exponent-1023,
+            number.fields.mantisse);
+
+}
 
 int
 main(void)
 {
 
-    bool all_tests_pass = test_runner();
+    bool all_tests_pass = test_runner();\
+    printf("size of ieee float 64 bit: %i\n", sizeof(ieee_float64));
+
+    ieee_float64 first = {
+            .number = 0.3
+    };
+    ieee_float64 second = {
+            .number = 3*0.1
+    };
+    printFloat(first);
+    printFloat(second);
+
     if (all_tests_pass)
     {
         return 0;
