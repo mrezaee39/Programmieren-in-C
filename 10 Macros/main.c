ac1408d05fb02434bf947cf86c4f4371498d9625
Lukas Einhaus <lukas.einhaus@uni-due.de>
2020-06-24 23:02:57 +0200

    add exercise 11 Routing

--- 10 Macros/main.c
+++ 10 Macros/main.c
@@ -1,19 +1,8 @@
-#include <stdint.h>
 #include "debug.h"
-
-int sum(int a, int b)
-{
-    int result = a + b;
-    return result;
-}
 
 int
 main(void)
 {
-    int s = sum(1,2);
-    long *a = &s;
-    uint8_t byte = 0x05;
-    char *c = (char *)&byte;
-    debug_print(DEBUG_LEVEL_ERROR, "%s: %i\n", "ich bin ein debug string!", s);
+    debug_print(DEBUG_LEVEL_ERROR, "%s\n", "ich bin ein debug string!");
     debug_print(DEBUG_LEVEL_WARNING, "%s\n", "ich bin ne warning!");
 }
